275310a2af053d1e8522e237a1b6cddae4018449
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-01-18 21:52:05 +0100

    add hover animation to SoundCard

--- src/lib/SoundCard.svelte
+++ src/lib/SoundCard.svelte
@@ -4,6 +4,6 @@
 	export let pad_data: PadData;
 </script>
 
-<div class="border rounded border-blue-500 p-4 w-48 h-28">
+<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 	<p>{pad_data.title}</p>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,37 +18,35 @@
 <div class="flex flex-col items-center mt-8">
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
 		<SoundCard pad_data={test_pad_data}></SoundCard>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<SoundCard pad_data={test_pad_data}></SoundCard>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
-			<p>Place holder</p>
-		</div>
-		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
 			<p>Place holder</p>
 		</div>
 	</div>
