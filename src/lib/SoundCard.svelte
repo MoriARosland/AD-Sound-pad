116b84f967629aa9c8cd055bf76edadae2a2d36c
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-01-18 21:41:52 +0100

    add soundcard component

---
+++ src/lib/SoundCard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { PadData } from '$lib/PadData';
+
+	export let pad_data: PadData;
+</script>
+
+<div class="border rounded border-blue-500 p-4 w-48 h-28">
+	<p>{pad_data.title}</p>
+</div>
