8bde627562133a110f1a6348e3ee6903d160da97
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-01-18 21:15:36 +0100

    Added grid layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,47 @@
-<div class="flex flex-col items-center justify-center">
-	<h1>AD Sound-pad</h1>
+<div class="flex flex-col items-center justify-center py-5">
+	<h1 class="text-2xl">AD Sound-pad</h1>
 	<p>
 		Github <a href="https://github.com/NamePending99/AD-Sound-pad">repository</a>
 	</p>
+</div>
+
+<div class="flex flex-col items-center mt-8">
+	<div class="grid grid-cols-4 gap-4">
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+		<div class="border rounded border-blue-500 p-4 w-48 h-28">
+			<p>Place holder</p>
+		</div>
+	</div>
 </div>
