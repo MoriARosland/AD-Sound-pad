040d90e5742c3b836a2836610d283e8c71f1e0ae
Mori Rosland MAC <moriar@stud.ntnu.no>
2024-01-18 22:34:39 +0100

    add audio playback

--- src/lib/SoundCard.svelte
+++ src/lib/SoundCard.svelte
@@ -2,8 +2,20 @@
 	import type { PadData } from '$lib/PadData';
 
 	export let pad_data: PadData;
+
+	// let audio: HTMLAudioElement;
+
+	function playAudio() {
+		const audio = new Audio('src/lib/assets/jas.m4a');
+		audio.currentTime = 0.68;
+		audio.play();
+	}
 </script>
 
-<div class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform">
+<audio src="src/lib/assets/jas.m4a"></audio>
+<div
+	on:click={playAudio}
+	class="border rounded border-blue-500 p-4 w-48 h-28 hover:scale-105 transition-transform"
+>
 	<p>{pad_data.title}</p>
 </div>
